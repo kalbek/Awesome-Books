111f543e20c8246677c884eaef650ec0b40b54a8
kalbek <access2kaleb@gmail.com>
2023-03-20 23:13:23 +0300

    correct linters errors for concatination

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 let books = [];
-const addButton = document.getElementById('add-books');
-const bookList = document.querySelector('.book-list');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const addButton = document.getElementById("add-books");
+const bookList = document.querySelector(".book-list");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
 // handle dynamically creating book list html content
 function createBookList() {
-  let singleBook = '';
-  bookList.innerHTML = '';
+  bookList.innerHTML = "";
   books.forEach((book) => {
-    if (books.title !== '' && book.author !== '') {
-      singleBook += `
+    if (book.title !== "" && book.author !== "") {
+      bookList.innerHTML += `
       <div class='book-title'>${book.title}</div>
         <div class='book-author'>${book.author} </div>
         <button class='removes' id='${book.id}'>Remove</button>
       <hr />
       `;
     }
-  });
-  bookList.innerHTML = singleBook;
-  // while creating a book element each time create event listener
-  // for every remove button of the book list html element
-  for (let i = 0; i < books.length; i += 1) {
-    const removeButton = document.getElementById(i.toString()); // remove0
-    removeButton.addEventListener('click', () => {
-      let bookId = removeButton.id.slice(6);
+    // while creating a book element each time create event listener
+    // for every remove button of the book list html element
+    const removeButton = document.getElementById(book.id.toString()); // remove0
+    removeButton.addEventListener("click", (e) => {
+      console.log(Number(e.target.id));
+      const bookId = Number(Number(e.target.id));
       removeBooks(bookId);
     });
-  }
+  });
 }
 // handle adding books to books array
 function addBooks(titles, authors) {
@@ -38,51 +35,38 @@ function addBooks(titles, authors) {
   // update book list html content with every action of adding books
   createBookList();
   title.focus();
-  localStorage.setItem('bookdata', JSON.stringify(books));
+  localStorage.setItem("bookdata", JSON.stringify(books));
 }
 //  function to remove books  // 0, 1, 2, 3,
 function removeBooks(bookId) {
-  books = books.filter((book) => book.id != bookId);
+  books = books.filter((book) => book.id !== bookId);
   // shift ids of books array elements starting from the removed element
   books.forEach((book) => {
     if (book.id > bookId) book.id -= 1;
   });
   // update book list html content with every action of removing books
-  createBookList(books);
-  localStorage.setItem('bookdata', JSON.stringify(books));
+  let singleBook = "";
+  bookList.innerHTML = "";
+  books.forEach((book) => {
+    if (books.title !== "" && book.author !== "") {
+      singleBook += `
+      <div class='book-title'>${book.title}</div>
+        <div class='book-author'>${book.author} </div>
+        <button class='removes' id='${book.id}'>Remove</button>
+      <hr />
+      `;
+    }
+  });
+  bookList.innerHTML = singleBook;
+  localStorage.setItem("bookdata", JSON.stringify(books));
   title.focus();
 }
-const titles = document.getElementById('title');
-const authors = document.getElementById('author');
+const titles = document.getElementById("title");
+const authors = document.getElementById("author");
 //   handle button click event for add books
-addButton.addEventListener('click', (e) => {
+addButton.addEventListener("click", (e) => {
   addBooks(titles, authors);
-  titles.value = '';
-  authors.value = '';
+  titles.value = "";
+  authors.value = "";
   e.preventDefault();
 });
-
-// preserve data on browsers on page load
-window.onload = () => {
-  // assigning form fields to a variable.
-  const form = document.querySelector('#form');
-  // handling event for every change in input field
-  form.addEventListener('input', () => {
-    const fields = { title, author, books };
-    // storing input values to a local storage.
-    fields.title = title.value;
-    fields.author = author.value;
-    localStorage.setItem('data', JSON.stringify(fields));
-  });
-  // parsing the data.
-  const data = JSON.parse(localStorage.getItem('data'));
-  const bookdata = JSON.parse(localStorage.getItem('bookdata'));
-  // assining values to input fields from local storage on page load.
-  if (data !== null && data !== undefined) {
-    title.value = data.title;
-    author.value = data.author;
-  }
-  books = bookdata;
-  createBookList();
-  title.focus();
-};
