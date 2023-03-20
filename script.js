422fc5c137d79774c4d113ed081814fc4f646ac2
kalbek <44664009+kalbek@users.noreply.github.com>
2023-03-20 14:44:35 +0300

    create functionality for removing books from list

--- script.js
+++ script.js
@@ -15,7 +15,15 @@ function main() {
               `;
     });
     bookList.innerHTML = singleBook;
- 
+    // while creating a book element each time create event litener
+    // for every remove button of the book list html element
+    for (let i = 0; i < books.length; i++) {
+      let removeButton = document.getElementById("remove" + i);
+      removeButton.addEventListener("click", () => {
+        let bookId = removeButton.id.slice(6);
+        removeBooks(bookId);
+      });
+    }
   }
   // handle adding books to books array
   function addBooks(titles, authors) {
@@ -27,7 +35,13 @@ function main() {
     // update book list html content with every action of adding books
     createBookList(books);
   }
- 
+  //  function to remove books
+  function removeBooks(bookId) {
+    books = books.filter((book) => book.id != bookId);
+   
+    // update book list html content with every action of removing books
+    createBookList(books);
+  }
   const titles = document.getElementById("title");
   const authors = document.getElementById("author");
   //   handle button click event for add books
