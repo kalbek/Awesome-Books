9e1576a925c9e9f87ef2c1084fa59f9caf6d1f0e
kalbek <access2kaleb@gmail.com>
2023-03-21 03:19:48 +0300

    fix usage of function before definition

--- script.js
+++ script.js
@@ -3,40 +3,6 @@ const addButton = document.getElementById('add-books');
 const bookList = document.querySelector('.book-list');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-// handle dynamically creating book list html content
-function createBookList() {
-  bookList.innerHTML = '';
-  books.forEach((book) => {
-    if (book.title !== '' && book.author !== '') {
-      bookList.innerHTML += `
-      <div class='book-title'>${book.title}</div>
-        <div class='book-author'>${book.author} </div>
-        <button class='removes' id='${book.id}'>Remove</button>
-      <hr />
-      `;
-    }
-  });
-  books.forEach((book) => {
-    // for every remove button of the book list html element
-    const removeButton = document.getElementById(book.id.toString()); // remove0
-    removeButton.addEventListener('click', () => {
-      const bookId = Number(Number(book.id));
-      removeBooks(bookId);
-    });
-  });
-}
-// handle adding books to books array
-function addBooks(titles, authors) {
-  books.push({
-    id: books.length,
-    title: titles.value,
-    author: authors.value,
-  });
-  // update book list html content with every action of adding books
-  createBookList();
-  title.focus();
-  localStorage.setItem('bookdata', JSON.stringify(books));
-}
 //  function to remove books  // 0, 1, 2, 3,
 function removeBooks(bookId) {
   // first remove event listener for the book
@@ -71,6 +37,40 @@ function removeBooks(bookId) {
   });
   localStorage.setItem('bookdata', JSON.stringify(books));
   title.focus();
+}
+// handle dynamically creating book list html content
+function createBookList() {
+  bookList.innerHTML = '';
+  books.forEach((book) => {
+    if (book.title !== '' && book.author !== '') {
+      bookList.innerHTML += `
+      <div class='book-title'>${book.title}</div>
+        <div class='book-author'>${book.author} </div>
+        <button class='removes' id='${book.id}'>Remove</button>
+      <hr />
+      `;
+    }
+  });
+  books.forEach((book) => {
+    // for every remove button of the book list html element
+    const removeButton = document.getElementById(book.id.toString()); // remove0
+    removeButton.addEventListener('click', () => {
+      const bookId = Number(Number(book.id));
+      removeBooks(bookId);
+    });
+  });
+}
+// handle adding books to books array
+function addBooks(titles, authors) {
+  books.push({
+    id: books.length,
+    title: titles.value,
+    author: authors.value,
+  });
+  // update book list html content with every action of adding books
+  createBookList();
+  title.focus();
+  localStorage.setItem('bookdata', JSON.stringify(books));
 }
 const titles = document.getElementById('title');
 const authors = document.getElementById('author');
