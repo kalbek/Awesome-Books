0ca559ebd329464965c8e9e02f262c3e0a9d1a0c
otegaa <ekiugbotega@gmail.com>
2023-03-21 15:22:08 +0100

    Add styles

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let books = [];
-const addButton = document.getElementById('add-books');
-const bookList = document.querySelector('.book-list');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const titles = document.getElementById('title');
-const authors = document.getElementById('author');
+const addButton = document.getElementById("add-books");
+const bookList = document.querySelector(".book-list");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const titles = document.getElementById("title");
+const authors = document.getElementById("author");
 class Books {
   constructor(id, title, author) {
     this.id = id;
@@ -15,7 +15,7 @@ class Books {
   static removeBooks = (bookId) => {
     // first remove event listener for the book
     const removeButton = document.getElementById(bookId.toString());
-    removeButton.removeEventListener('click', () => {});
+    removeButton.removeEventListener("click", () => {});
     books = books.filter((book) => book.id !== bookId);
     // shift ids of books array elements starting from the removed element
     books.forEach((book) => {
@@ -23,11 +23,13 @@ class Books {
     });
     // update book list html content with every action of removing books
     // replace with updating
-    bookList.innerHTML = '';
+    bookList.innerHTML = "";
     books.forEach((book) => {
       bookList.innerHTML += `
       <div class='flex'>
-      <div class='flex-spaced w-100' id='${book.id % 2 === 0 ? 'evens' : 'odds'}'>
+      <div class='flex-spaced w-100' id='${
+        book.id % 2 === 0 ? "evens" : "odds"
+      }'>
         <div class ='flex book-title-authors'>
           <div class='book-title'>"${book.title}" by </div>
           <div class='book-author'>&nbsp;${book.author} </div>
@@ -43,22 +45,24 @@ class Books {
       // while creating a book element each time create event listener
       // for every remove button of the book list html element
       const removeButton = document.getElementById(book.id.toString()); // remove0
-      removeButton.addEventListener('click', () => {
+      removeButton.addEventListener("click", () => {
         const bookId = Number(Number(book.id));
         Books.removeBooks(bookId);
       });
     });
-    localStorage.setItem('bookdata', JSON.stringify(books));
+    localStorage.setItem("bookdata", JSON.stringify(books));
     title.focus();
   };
 
   // handle dynamically creating book list html content
   static createBookList = () => {
-    bookList.innerHTML = '';
+    bookList.innerHTML = "";
     books.forEach((book) => {
       bookList.innerHTML += `
       <div class='flex'>
-        <div class='flex-spaced w-100' id='${book.id % 2 === 0 ? 'evens' : 'odds'}'>
+        <div class='flex-spaced w-100' id='${
+          book.id % 2 === 0 ? "evens" : "odds"
+        }'>
           <div class ='flex book-title-authors'>
             <div class='book-title'>"${book.title}" by </div>
             <div class='book-author'>&nbsp;${book.author} </div>
@@ -75,7 +79,7 @@ class Books {
       // for every remove button of the book list html element
       const removeButton = document.getElementById(book.id.toString());
       if (removeButton !== null) {
-        removeButton.addEventListener('click', () => {
+        removeButton.addEventListener("click", () => {
           const bookId = Number(Number(book.id));
           Books.removeBooks(bookId);
         });
@@ -84,11 +88,13 @@ class Books {
   };
 
   static createBookList = () => {
-    bookList.innerHTML = '';
+    bookList.innerHTML = "";
     books.forEach((book) => {
       bookList.innerHTML += `
       <div class='flex'>
-        <div class='flex-spaced w-100' id='${book.id % 2 === 0 ? 'evens' : 'odds'}'>
+        <div class='flex-spaced w-100' id='${
+          book.id % 2 === 0 ? "evens" : "odds"
+        }'>
           <div class ='flex book-title-authors'>
             <div class='book-title'>"${book.title}" by </div>
             <div class='book-author'>&nbsp;${book.author} </div>
@@ -104,7 +110,7 @@ class Books {
       // for every remove button of the book list html element
       const removeButton = document.getElementById(book.id.toString());
       if (removeButton !== null) {
-        removeButton.addEventListener('click', () => {
+        removeButton.addEventListener("click", () => {
           const bookId = Number(Number(book.id));
           Books.removeBooks(bookId);
         });
@@ -122,31 +128,31 @@ class Books {
     // update book list html content with every action of adding books
     Books.createBookList();
     title.focus();
-    localStorage.setItem('bookdata', JSON.stringify(books));
+    localStorage.setItem("bookdata", JSON.stringify(books));
   };
 }
 //   handle button click event for add books
-addButton.addEventListener('click', (e) => {
+addButton.addEventListener("click", (e) => {
   Books.addBooks(titles, authors);
-  titles.value = '';
-  authors.value = '';
+  titles.value = "";
+  authors.value = "";
   e.preventDefault();
 });
 // preserve data on browsers on page load
 window.onload = () => {
   // assigning form fields to a variable.
-  const form = document.querySelector('#form');
+  const form = document.querySelector("#form");
   // handling event for every change in input field
-  form.addEventListener('input', () => {
+  form.addEventListener("input", () => {
     const fields = { title, author, books };
     // storing input values to a local storage.
     fields.title = title.value;
     fields.author = author.value;
-    localStorage.setItem('data', JSON.stringify(fields));
+    localStorage.setItem("data", JSON.stringify(fields));
   });
   // parsing the data.
-  const data = JSON.parse(localStorage.getItem('data'));
-  const bookdata = JSON.parse(localStorage.getItem('bookdata'));
+  const data = JSON.parse(localStorage.getItem("data"));
+  const bookdata = JSON.parse(localStorage.getItem("bookdata"));
   // assining values to input fields from local storage on page load.
   if (data !== null && data !== undefined) {
     title.value = data.title;
